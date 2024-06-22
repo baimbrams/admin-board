37b535a527d915fc1311dae31c9298b171aff5bd
baimbrams <baimz.bramz@gmail.com>
2024-06-22 12:22:49 +0700

    "Beranda Page Admin"

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -43,7 +43,7 @@ export const StoreModal = () => {
       const response = await axios.post("/api/stores", values);
       console.log(response.data);
       toast.success("Berhasil membuat toko");
-      // window.location.assign(`/${response.data.id}`);
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("Gagal Membuat Toko");
     } finally {
