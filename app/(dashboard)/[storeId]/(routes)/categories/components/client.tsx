373f25bd0e2c8d6b7b1a4f7dd6be02167e820fb2
Bram <baimz.bramz@gmail.com>
2024-06-26 11:35:53 +0700

    "Feature Kategori"

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { CategoryColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface categoriesClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoriesClient: React.FC<categoriesClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Category (${data.length})`}
+          description="Atur Kategori Toko"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Tambah Baru
+        </Button>
+      </div>
+      <Separator />
+      <DataTable data={data} columns={columns} searchKey="name" />
+      <Separator />
+      <Heading title="API" description="API untuk Categories" />
+      <ApiList namaIndikator="categories" idIndikator="categoryId" />
+    </>
+  );
+};
